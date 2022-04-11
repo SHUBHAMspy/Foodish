2568bb426dd9223b2259b8f2fd98458c34517d69
Shubham Pandey <shubhampandey1964@gmail.com>
2022-04-11 13:08:03 +0530

    Refactored Redux & Added  Selectors

--- redux/cartSlice.js
+++ redux/cartSlice.js
@@ -1,11 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createDraftSafeSelector, createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     products: [],
-    quantity: 0,
-    total: 0,
   },
 
   // We can write the case reducers as functions inside of an object, instead of having to write a switch/case statement
@@ -14,18 +12,48 @@ const cartSlice = createSlice({
   // reducers: an object where the keys are strings, and the values are "case reducer" functions that will handle specific actions
   reducers:{
     addProduct: (state,action) => {
-      state.products.push(action.payload); // 1st push the incomingproduct 
-      state.quantity += 1; // then increase their quantity
-      state.total += action.payload.price * action.payload.quantity;
+      const products = state.products;
+      const itemToAdd = action.payload;
+      
+      // To check for existing product in cart
+      const itemExists = products.find((item) => item._id === itemToAdd._id);
+      if (itemExists) {
+        itemExists.quantity++;
+      } else {
+        products.push({ ...itemToAdd, quantity: 1 });
+      }
+      // state.products.push(action.payload); // 1st push the incomingproduct 
+      // state.quantity += 1; // then increase their quantity
+      // state.total += action.payload.price * action.payload.quantity;
     },
-    reset: (state) => {
-      state.products = [];
-      state.quantity = 0;
-      state.total = 0;
+    removeProduct: (state,action) => {
+      const products = state.products;
+      const itemToRemove = action.payload;
+      const item = products.find((item) => item._id === itemToRemove._id);
+      if (item.quantity === 1) {
+        const index = products.findIndex((item) => item._id === itemToRemove._id);
+        products.splice(index, 1);
+      } else {
+        item.quantity--;
+      }
     }
+    // reset: (state) => {
+    //   state.products = [];
+    //   state.quantity = 0;
+    //   state.total = 0;
+    // }
   }
 
 })
+export const selectProducts = (state) => state.cart.products
+export const selectProductsCount = createDraftSafeSelector([selectProducts],
+  (products) => products.reduce((accumulatedQuantity,product) => accumulatedQuantity + product.quantity,0)
+);
+export const selectCartTotal = createDraftSafeSelector([selectProducts],
+  (products) => products.reduce((accumulatedPrice,product) => accumulatedPrice + product.quantity * product.price,0)
+);
+//console.log(selectProducts());
 //The complete reducer function is available as slice.reducer
-export const {addProduct,reset} = cartSlice.actions;
-export default cartSlice.reducer;
+export const {addProduct,removeProduct} = cartSlice.actions;
+const cartReducer = cartSlice.reducer;
+export default cartReducer;
