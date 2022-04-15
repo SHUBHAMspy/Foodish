ca1d39f13002c20403748e0525f6205e16b61523
Shubham Pandey <shubhampandey1964@gmail.com>
2022-04-15 14:02:44 +0530

    Orders api

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,28 @@
+import nc from 'next-connect';
+import dbConnect from '../../../config/dbConnect';
+import Order from '../../../models/order';
+
+const handler = nc();
+
+await dbConnect();
+
+handler.get(async(req,res) => {
+  const {query: { id }} = req;
+  try {
+    const order = await Order.findById(id);
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+handler.put(async(req,res) => {
+  const {query: { id }} = req;
+  try {
+    const order = await Order.findByIdAndUpdate(id,req.body,{new:true,});
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+})
+export default handler;
